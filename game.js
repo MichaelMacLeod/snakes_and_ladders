e813545d73e46c7daae36218000a900553bf83c6
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-01-28 15:14:52 +0000

    Added working tests for Board, Dice, Player, Game. Working on turn counter.

--- game.js
+++ game.js
@@ -1,11 +1,39 @@
-var Game = function(){
+var Game = function(board, dice){
   this.players = []
+  this.board = board;
+  this.dice = dice;
+  this.turn = 0;
 };
 
 Game.prototype = {
   addPlayer: function(player) {
     this.players.push(player);
-  }
+  },
+  checkForWinners: function(){
+    for (player of this.players) {
+      if (player.currentPosition >= 99) {
+        return player.name;
+      }
+    }
+    return false;
+  },
+  incrementTurn: function(){
+    this.turn += 1;
+    if (this.turn == this.players.length) {
+      this.turn = 0;
+    }
+  },
+  nextTurn: function(){
+    var player = this.players[this.turn];
+    var roll = this.dice.roll();
+    player.currentPosition += roll;
+    var winner = this.checkForWinners();
+    if (winner) {
+      return 'The winner is ' +player.name;
+    }
+    this.incrementTurn();
+    this.nextTurn();
+  },
 }
 
 module.exports = Game;
--- snakes_game_test.js
+++ snakes_game_test.js
@@ -4,6 +4,23 @@ var Dice = require ('./dice');
 var Player = require ('./player');
 var Board = require ('./board');
 
+//0. Default game start position: 
+
+function createGame(){
+  var player1 = new Player('player 1');
+  var player2 = new Player('player 2');
+  var player3 = new Player('player 3');
+  var player4 = new Player('player 4');
+  var board = new Board();
+  var dice = new Dice();
+  var game = new Game(board, dice);
+  game.addPlayer(player1);
+  game.addPlayer(player2);
+  game.addPlayer(player3);
+  game.addPlayer(player4);
+  return game;
+}
+
 //1. Board 
 
 describe("Board,", function(){
@@ -60,9 +77,49 @@ describe("Game", function(){
     var board = new Board();
     var game = new Game(board);
     assert.equal(100, game.board.squares.length)
+  });
+  it('should have a dice', function(){
+    var board = new Board();
+    var dice = new Dice();
+    var game = new Game(board, dice);
+    assert.equal(6, game.dice.sides)
+  });
+  it('should have a winner if player has currentPosition of 99', function(){
+    game = createGame();
+    game.players[0].currentPosition = 99,
+    winner = game.checkForWinners();
+    assert.equal('player 1', winner);
+    });
+  it("should have no winner if no player's currentPosition of 99", function(){
+    game = createGame();
+    var winner = game.checkForWinners();
+    assert.equal(false, winner);
+    });
+  //5. Turn counter: 
+  it('should have turn counter starting at zero', function(){
+    game = createGame();
+    assert.equal(0, game.turn);
+  });
+  it('should increment turn by 1 when next turn is called', function(){
+    game = createGame();
+    game.incrementTurn();
+    assert.equal(1, game.turn);
+  })
+  it('should make the next turn 0 when the current turn is 3', function(){
+    game = createGame();
+    game.turn = 3;
+    game.incrementTurn();
+    assert.equal(0, game.turn);
+  })
+  it("should return 'The winner is player 1' if they have won", function(){
+    game = createGame();
+    game.players[0].currentPosition = 98;
+    result = game.nextTurn();
+    assert.equal('The winner is player 1', result);
   })
 });
 
+//5. Turn counter: 
 
 
 
